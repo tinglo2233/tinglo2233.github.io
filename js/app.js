510d23c4e5313e1d4accad3205eac7d8a9f15d0e
Fonserbc <Fonserbc@gmail.com>
2019-06-19 15:26:29 +0200

    Working on youtube embed

--- js/app.js
+++ js/app.js
@@ -1,12 +1,63 @@
 $(function() {
     
+	// This code loads the IFrame Player API code asynchronously.
+	//var tag = document.createElement('script');
+
+	//tag.src = "https://www.youtube.com/iframe_api";
+	//var firstScriptTag = document.getElementsByTagName('script')[0];
+	//firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+	// 3. This function creates an <iframe> (and YouTube player)
+	//    after the API code downloads.
+	var ytPlayers = {};
+	function onYouTubeIframeAPIReady() {
+		console.log("hello youtube");
+		ytPlayers["strings"] = new YT.Player('strings-iframe', {
+			listType: 'playlist',
+			list: 'PLjd2GXZgLHfQoco2vfF0VxVsydX3Xc6Kn',
+			enablejsapi: 1
+
+			/*height: '390',
+			width: '640',
+			videoId: 'M7lc1UVf-VE',
+			events: {
+				'onReady': onPlayerReady,
+				'onStateChange': onPlayerStateChange
+			}*/
+		});
+		ytPlayers["offscore"] = new YT.Player('offscore-iframe', {
+			listType: 'playlist',
+			list: 'PLjd2GXZgLHfRwOLh_R7wkPachGP9N-tCb',
+			enablejsapi: 1
+		});
+		ytPlayers["originals"] = new YT.Player('originals-iframe', {
+			listType: 'playlist',
+			list: 'PLjd2GXZgLHfSm-sCS-PXkb-UndpZ_JyMC',
+			enablejsapi: 1
+		});
+		ytPlayers["standards"] = new YT.Player('standards-iframe', {
+			listType: 'playlist',
+			list: 'PLjd2GXZgLHfR6prtzs7rSOtFwd6Nm5Dks',
+			enablejsapi: 1
+		});
+	}
+
+
     // Globals
 	window.stage = "";
     window.stageInfos = [];
+	window.projectInfos = [];
+
+	const aboutStage = "about";
+	const musicStage = "music";
+	const projectStage = "projects";
+	const newsStage = "news";
     
-    var stageNames = ["about", "music", "projects", "news"];
-    var stageHasSideInfo = {"about": true, "music": true, "projects": false, "news": false};
+    var stageNames = [aboutStage, musicStage, projectStage, newsStage];
+    var stageHasSideInfo = {aboutStage: true, musicStage: true, projectStage: false, newsStage: false};
     var stageMap = {};
+	var projectNames = ["strings", "offscore", "originals", "standards"];
+	var projectMap = {};
     var contentView = $("#content-div");
     var menuView = $("#menu-div");
 	var fbContainer = $("#facebook-container");
@@ -27,8 +78,27 @@ $(function() {
         stage.button.on("click", function () {window.setStage(stage.name);});
         stageMap[name] = stage;
     }
+
+	for (let i = 0; i < projectNames.length; ++i) {
+        let name = projectNames[i];
+        
+        let project = {
+            name: name,
+            button: $("#"+name+"-button"),
+            content: $("#"+name+"-project-content"),
+			iframe: $("#"+name+"-iframe")
+        };
+        
+        window.projectInfos.push(project);
+        project.button.on("click", function () {window.setProject(project.name);});
+        projectMap[name] = project;
+    }
     
-    window.setStage = function(stage) {
+    window.setStage = function(stage)
+	{
+		if (window.project != null) 
+			window.setProject(null);
+
         if (window.stage == stage) {
             return;
         }
@@ -48,8 +118,7 @@ $(function() {
                 button.addClass("active");
             }
 			else {
-				if (!view.hasClass("hidden"))
-					view.addClass("hidden");
+				view.addClass("hidden");
                 content.addClass("hidden");
                 button.removeClass("active");
             }
@@ -79,11 +148,44 @@ $(function() {
 
 		
 
-		if (stage == "news") {
+		if (stage == newsStage) {
 			ShowFacebook();
 			//setTimeout(ShowFacebook,512);
 		}
     }
+
+	window.setProject = function (project) {
+		if (project != null && window.stage != projectStage) {
+			window.setStage(projectStage);
+		}
+
+		// Update Visibilites
+		for (let i = 0; i < window.projectInfos.length; ++i) {
+            let button = window.projectInfos[i].button;
+			let name = window.projectInfos[i].name;
+            let content = window.projectInfos[i].content;
+			
+			if (name == project) {
+                content.removeClass("hidden");
+                button.addClass("hidden");
+            }
+			else if (project == null) {
+                content.addClass("hidden");
+                button.removeClass("hidden");
+            }
+			else {
+                content.addClass("hidden");
+                button.addClass("hidden");
+			}
+
+			let iframePlayer = ytPlayers[name];
+			if (iframePlayer != null) {
+				iframePlayer.pauseVideo();
+			}
+		}
+
+		window.project = project;
+	}
 
 	var lastFbWidth = 0;
 	var lastFbHeight = 0;
@@ -112,7 +214,7 @@ $(function() {
         bgImages[i].src = "img/"+stageNames[i]+".jpg";
     }
     
-	var wantedLandingStage = "about";
+	var wantedLandingStage = aboutStage;
 
 	var currentHash = location.hash;
 	currentHash = currentHash.substr(1, currentHash.length - 1);
@@ -120,5 +222,7 @@ $(function() {
 	for (var i = 0; i < stageNames.length; ++i) {
 		if (stageNames[i] == currentHash) wantedLandingStage = stageNames[i];
 	}
+
     window.setStage(wantedLandingStage);
+	window.setProject(null);
 });
